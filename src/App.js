e07c98bc538627b830d1f34d70a12537617c0df5
EdwardSuryaputra <65845434+EdwardSuryaputra@users.noreply.github.com>
2022-02-20 19:06:49 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,7 +11,13 @@ import 'firebase/compat/analytics';
 import { useAuthState } from 'react-firebase-hooks/auth';
 
 firebase.initializeApp({
-  //put your config here
+  apiKey: "AIzaSyBFGMiWOIfGIj3nvdAe1Dv54oEn02gekAg",
+  authDomain: "superchat-app-5dba0.firebaseapp.com",
+  projectId: "superchat-app-5dba0",
+  storageBucket: "superchat-app-5dba0.appspot.com",
+  messagingSenderId: "761604098624",
+  appId: "1:761604098624:web:c8c1f426cb72d2abe6eda5",
+  measurementId: "G-0LT8W0DY9P"
 })
 
 const auth = firebase.auth();
